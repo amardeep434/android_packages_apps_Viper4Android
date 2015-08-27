6f9909114230f7cff8a3c2c98ab82ec72a548d4b
Jason H Weitzel <jsnweitzel@gmail.com>
2015-08-27 18:11:35 -0400

    add v4a backup 	new file:   system/addon.d/91-v4a.sh 	modified:   viperfx.mk

--- viperfx.mk
+++ viperfx.mk
@@ -2,7 +2,8 @@ PRODUCT_PACKAGES += \
     Viper4Android
 
 PRODUCT_COPY_FILES += \
-     packages/apps/Viper4Android/system/etc/audio_effects.conf:system/etc/audio_effects.conf
+     packages/apps/Viper4Android/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
+     packages/apps/Viper4Android/system/addon.d/91-v4a.sh:system/addon.d/91-v4a.sh
 
 PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=false \
